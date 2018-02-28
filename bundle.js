aba03268a5aea549e40a0c7cd3fddae1ab2cd305
Colin Travis <colint@cybertravis.us>
2018-02-27 22:11:59 -0600

    implemented browserify

---
+++ bundle.js
@@ -0,0 +1,6 @@
+var cookies = require('js-cookie');
+var $ = require('jquery');
+window.$ = window.jQuery = $;
+
+var prompt = require('./public/js/prompt.js')
+var prompt = require('./public/js/interactivePrompt.js')
