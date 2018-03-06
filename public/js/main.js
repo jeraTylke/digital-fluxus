eee5efb2766cbe3ae17661949287ee0928a13ca4
Jeralise Tylke <jtylke@Jeralises-MacBook-Pro.local>
2018-03-06 00:54:28 -0600

    more CSS style. refer to color.html and beatIP.html for style code

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,29 @@ var prompt = require('./public/js/interactivePrompt.js')
 ==UNIVERSAL SCRIPTS==
 =====================
 */
+var backgroundColors = [];
+
 var url = location.pathname;
+
+var interactivePrompts = ["bookIP.html","modernIP.html","moodIP.html","beatIP.html","storyIP.html","penIP.html","nightIP.html"];
+
+var basicPrompts = ['flowers.html','jump.html','shakeHands.html','race.html','lights.html','lemons.html','happen.html','time.html','floor.html','eyes.html','color.html','drums.html']
+
+for (var i = 0; i < interactivePrompts.length; i++) {
+  if (url.indexOf(interactivePrompts[i]) > -1) {
+    var backgroundColors = ["rgb(190, 70, 102)"];
+    changeBackgroundColor(backgroundColors);
+  } else if (url.indexOf(basicPrompts[i]) > -1) {
+    var backgroundColors = ["#ff0000","#00ff00","#0000ff"];
+    changeBackgroundColor(backgroundColors);
+  }
+}
+
+function changeBackgroundColor(randomColors){
+  var randColor = Math.floor(Math.random()* backgroundColors.length)
+  $(".mainBody").css('background-color', randomColors[randColor]);
+
+}
 
 var promptPages = ["moodIP","color","nightIP"]
 
